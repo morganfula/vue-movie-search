8ae37e243eb0fa01ed7f0f69171899ecbf1f8afa
morganfula <morgan.fula@me.com>
2020-12-30 12:15:23 +0100

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="App-header">
+    <h2>{{ title }}</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: ['title'],
+  setup() {},
+};
+</script>
+
+<style></style>
